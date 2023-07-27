af0664abca412c1b8685664ebd246db08389250f
Andrius <andriuscanis@gmail.com>
2023-07-27 15:16:05 +0300

    page404 styling

--- src/pages/page404/Page404.jsx
+++ src/pages/page404/Page404.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import { Button } from "../../components/button/Button";
 import logo from "../../assets/img/oops.png";
+import style from "../page404/Page404.module.css";
 
 export function Page404() {
   return (
-    <div>
+    <div className={style.page404}>
       <img src={logo} alt="oopsImg" />
       <h1>404</h1>
       <p>Page not found.</p>
       <Link to="/" >
-        Go home
+        <Button text="Go home" />
       </Link>
     </div>
   )
