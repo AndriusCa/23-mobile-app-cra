0bfabf2632ab18e9de4750b6806d7cf44ec70dee
Andrius <andriuscanis@gmail.com>
2023-07-27 18:23:14 +0300

    navigation dots added

--- src/pages/feature/Feature1.jsx
+++ src/pages/feature/Feature1.jsx
@@ -1,22 +1,24 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../components/button/Button";
+import { NavDots } from "../../components/navDots/NavDots";
+import { Skip } from "../../components/skip/Skip";
 import logo from "../../assets/img/organic.png";
 import style from "../feature/Feature.module.css";
 
 export function Feature1() {
   return (
     <div className={style.feature}>
-      <Link to="/register">Skip</Link>
-      <img className={style.featureImg} src={logo} alt="organicVeg"/>
+      <Skip />
+      <img className={style.featureImg} src={logo} alt="organicVeg" />
       <h1>Feature1</h1>
       <p>Lorem ipsum dolor sit amet consectetur.</p>
       <div>
-        <Link to="/register">0</Link>
-        <Link to="/register">0</Link>
-        <Link to="/register">0</Link>
+        <Link>
+          <NavDots />
+        </Link>
       </div>
       <Link to="/feature2">
-        <Button text="Continue"/>
+        <Button text="Continue" />
       </Link>
     </div>
   )
--- src/pages/feature/Feature2.jsx
+++ src/pages/feature/Feature2.jsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../components/button/Button";
+import { NavDots } from "../../components/navDots/NavDots";
+import { Skip } from "../../components/skip/Skip";
 import logo from "../../assets/img/veggie.png";
 import style from "../feature/Feature.module.css";
 
 export function Feature2() {
   return (
     <div className={style.feature}>
-      <Link to="/register">Skip</Link>
+      <Skip />
       <img className={style.featureImg} src={logo} alt="organicVeg" />
       <h1>Feature2</h1>
       <p>Lorem ipsum dolor sit amet consectetur.</p>
       <div>
-        <Link to="/register">0</Link>
-        <Link to="/register">0</Link>
-        <Link to="/register">0</Link>
+        <Link>
+          <NavDots />
+        </Link>
       </div>
       <Link to="/feature3">
         <Button text="Continue" />
